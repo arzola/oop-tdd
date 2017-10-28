261872bb06c3fcd13bd59648901158875cdd9a97
Oscar Arzola <oscararzola@gmail.com>
2017-10-28 08:49:12 -0500

    Fix tests

--- tests/ComputerTest.php
+++ tests/ComputerTest.php
@@ -7,6 +7,8 @@ class ComputerTest extends TestCase
 {
     public function test_if_late_static_binding_works()
     {
+        $pc = Computer::getBrand();
+        $this->assertEquals('PC', $pc);
         $name = \LateStatic\Mac::getBrand();
         $this->assertEquals('Mac', $name);
     }
